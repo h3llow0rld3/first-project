d8f519f4eadd80611b5205727275410e3e7d6af4
h3llow0rld3 <115990505+h3llow0rld3@users.noreply.github.com>
2022-10-31 09:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+let title = document.getElementById('title');
+
+title.classList.add('rotate');
+title.style.transition = '4s';
+
 setTimeout(() => {
-  let title = document.getElementById('title');
+  title.style.transition = '0.3s';
+  title.style.transition = '';
+}, 4000);
 
-  title.classList.add('rotate');
-  title.style.transition = '4s';
+let mouseDown = false;
 
-  setTimeout(() => {
-    title.style.transition = '0.3s';
-  }, 4000);
-}, 0);
+let img = document.getElementById('screamer');
 
+let resizeImg = function() {
+  let cloneTitleHeight = document.documentElement.clientHeight;
+  let cloneTitleWidth = document.documentElement.clientWidth;
+  
+  img.style.left = Math.round(cloneTitleWidth / 2 - img.offsetWidth / 2) + 'px';
+  img.style.top = Math.round(cloneTitleHeight / 2 - img.offsetHeight / 2) + 'px';
+}
+
+document.body.onresize = resizeImg;
 
 var sound = new Howl({
   src: ['screamer.mp3'],
@@ -16,8 +28,6 @@ var sound = new Howl({
 });
 
 function screamer() {
-  let img = document.getElementById('screamer');
-
   document.body.style.backgroundImage = 'none';
   
   for (let el of document.body.querySelectorAll('*:not(img)')) {
@@ -29,6 +39,6 @@ function screamer() {
 
   setTimeout(() => {
     img.hidden = false;
+    resizeImg();
   }, 1170);
 }
-
